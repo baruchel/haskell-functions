3c33f1a3ed631ab619ad655895bc18eafed82d95
Thomas Baruchel <baruchel@gmx.com>
2016-10-02 10:34:08 +0200

    First commit.

---
+++ Permutations.hs
@@ -0,0 +1,33 @@
+module Permutations where
+
+-- A module for quickly indexing permutations or building them
+-- from their index.
+--
+-- Syntax:
+--   > permIndex [5,4,2,1,8,7,0,6,3]
+--   223435
+--   > permNth 9 223435
+--   [5,4,2,1,8,7,0,6,3]
+
+factorials = scanl (*) 1 [1..]
+
+factorial :: Int -> Integer
+factorial n = factorials!!n
+
+
+permIndex :: [Int] -> Integer
+permIndex l = permIndex' ((length l)-1) l 0
+    where permIndex' :: Int -> [Int] -> Integer -> Integer
+          permIndex' 0 _ s = s
+          permIndex' n (h:t) s = permIndex' (n-1)
+                                            [if x<h then x else x-1 | x <- t]
+                                            (s + (toInteger h)*factorial n)
+
+
+permNth :: Int -> Integer -> [Int]
+permNth k n = permNth' n [] [0..(k-1)] (k-1)
+    where permNth' :: Integer -> [Int] -> [Int] -> Int -> [Int]
+          permNth' 0 p (rh:_) _ = reverse (rh:p)
+          permNth' n p r f = permNth' (mod n fact) (qh:p) (q ++ qt) (f-1)
+              where fact = factorial f
+                    (q,qh:qt) = splitAt (fromIntegral $ div n fact) r
---
+++ Recurrence.hs
@@ -0,0 +1,33 @@
+module Recurrence where
+
+-- A module for computing linear recurrence relation.
+--
+-- Syntax:
+--   > findRecurrence [1,1,3,5,8,13,21,34,55,89,144]
+--   [1,-1,-1]
+
+
+import Data.Ratio
+import Data.List (dropWhileEnd)
+
+findRecurrence l =
+  findRecurrence' l [0%1] [1%1] 0
+    where
+      mmax = (div (length l) 2)
+      findRecurrence' l q1 q2 b
+        | null l           = normalize q2
+        | head l == 0      = findRecurrence' (tail l) q1 q2 (b+1)
+        | length q2 > mmax = [0]
+        | otherwise        =
+            findRecurrence' (tail $ invert l) q2 
+              (dropWhileEnd (0 ==) $
+                take (max (length q2) (b + length q1))
+                  (zipWith (+)
+                    ((map (\x -> x / (head l)) q2) ++ (repeat 0))
+                    ((replicate b 0) ++ q1 ++ (repeat 0)))) 1
+      invert l = foldl (\a b -> a ++ [- (sum $ zipWith (*) a b)/(head l)])
+                       [1%1 / (head l)]
+                       (tail $ scanl (\a b -> b:a) [] (tail l))
+      normalize l = let c = (\x -> if (head l)<0 then -x else x)
+                              (foldl lcm 1 (map denominator l))%1
+          in map (numerator . (c*)) l
