c2a67afdd3332eafd7ccea5ab26497b704dcbcd7
Thomas Baruchel <baruchel@gmx.com>
2016-10-02 12:31:07 +0200

    Added an alternate version for discussing.

--- Recurrence.hs
+++ Recurrence.hs
@@ -31,3 +31,30 @@ findRecurrence l =
       normalize l = let c = (\x -> if (head l)<0 then -x else x)
                               (foldl lcm 1 (map denominator l))%1
                       in map (numerator . (c*)) l
+
+-- alternate version where addition is recursive
+
+findRecurrence2 l =
+  findRecurrence' l [0%1] [1%1] 0
+    where
+      mmax = (div (length l) 2)
+      findRecurrence' l q1 q2 b
+        | null l           = normalize q2
+        | head l == 0      = findRecurrence' (tail l) q1 q2 (b+1)
+        | length q2 > mmax = [0]
+        | otherwise        =
+            findRecurrence' (tail $ invert l) q2 (add q1 q2 (head l) b []) 1
+      invert l = foldl (\a b -> a ++ [- (sum $ zipWith (*) a b)/(head l)])
+                       [1%1 / (head l)]
+                       (tail $ scanl (\a b -> b:a) [] (tail l))
+      add [] [] _ _ acc = reverse $ dropWhile (0==) acc
+      add [] (q2h:q2s) inva _ acc = add [] q2s inva 0 (q2h/inva:acc)
+      add (q1h:q1s) [] _ 0 acc = add q1s [] 1 0 (q1h:acc)
+      add q1 [] _ b acc = add q1 [] 1 (b-1) (0:acc)
+      add (q1h:q1s) (q2h:q2s) inva 0 acc =
+        add q1s q2s inva 0 (q2h/inva + q1h : acc)
+      add q1 (q2h:q2s) inva b acc =
+        add q1 q2s inva (b-1) (q2h/inva : acc)
+      normalize l = let c = (\x -> if (head l)<0 then -x else x)
+                              (foldl lcm 1 (map denominator l))%1
+                      in map (numerator . (c*)) l
